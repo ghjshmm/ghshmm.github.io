11eb7e7ecaffb207212ef52fbc3a36cceee1f5fb
HMM <github.hmm@gmail.com>
2025-10-27 11:24:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+  function armazenarValor(){
+      
+  var userInput = document.getElementById("userInput").value;
+    
+    console.log("O valor da variável é: " + userInput);
+
+    document.getElementById("valorInserido").innerText = "O valor inserido é " + userInput; 
+
+  }
